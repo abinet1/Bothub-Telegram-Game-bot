dca941d70c988fa747b34c05025ce1fe9fa3c0fe
abinet <abinet>
2024-08-10 00:14:32 +0300

    feat: compliting implimantation for the componants

--- src/App.tsx
+++ src/App.tsx
@@ -24,13 +24,6 @@ function App() {
           >
             Open regular modal
           </button>
-          <button
-            className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-            type="button"
-            onClick={() => setIsOpen(priv=>priv+1)}
-          >
-            Open regular modal
-          </button>
           <Modal 
             isOpen={isOpenModel} 
             onClose={()=>{
@@ -38,6 +31,13 @@ function App() {
               setIsOpenModel(false)
             }}
           />
+          <button
+            className="absolute bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+            type="button"
+            onClick={() => setIsOpen(priv=>priv+1)}
+          >
+            Open regular modal
+          </button>
         </>
       }
     </>
