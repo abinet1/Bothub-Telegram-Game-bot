06856b95e72b26bc3efbc08b10292af0419a6dba
abinet <abinet>
2024-08-10 00:19:53 +0300

    feat: compliting implimantation for the componants

--- src/App.tsx
+++ src/App.tsx
@@ -7,39 +7,44 @@ function App() {
   const [isOpenModel, setIsOpenModel] = React.useState(false)
   return (
     <>
-      {isOpen===1&&
-        <WelcomeScreen />}
-      {isOpen===2&&
-        <WelcomeScreenTwo />}
-      {isOpen===3&&
-        <WelcomeScreenThree />}
-      {isOpen===4&&
-      <TutorialScreens />}
-      {isOpen===5&&
-        <>
-          <button
-            className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-            type="button"
-            onClick={() => setIsOpen(priv=>priv+1)}
-          >
-            Open regular modal
-          </button>
-          <Modal 
-            isOpen={isOpenModel} 
-            onClose={()=>{
-              setIsOpen(1)
-              setIsOpenModel(false)
-            }}
-          />
-          <button
-            className="absolute bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-            type="button"
-            onClick={() => setIsOpen(priv=>priv+1)}
-          >
-            Open regular modal
-          </button>
-        </>
-      }
+    <button
+      style={{zIndex: 1000}} 
+          className="absolute bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+          type="button"
+          onClick={() => setIsOpen(priv=>priv>5?1:priv+1)}
+        >
+          Open regular modal
+        </button>
+      <div className='relative'>
+        
+        {isOpen===1&&
+          <WelcomeScreen />}
+        {isOpen===2&&
+          <WelcomeScreenTwo />}
+        {isOpen===3&&
+          <WelcomeScreenThree />}
+        {isOpen===4&&
+        <TutorialScreens />}
+        {isOpen===5&&
+          <>
+            <button
+              className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+              type="button"
+              onClick={() => setIsOpen(priv=>priv+1)}
+            >
+              Open regular modal
+            </button>
+            <Modal 
+              isOpen={isOpenModel} 
+              onClose={()=>{
+                setIsOpen(1)
+                setIsOpenModel(false)
+              }}
+            />
+            
+          </>
+        }
+      </div>
     </>
   )
 }
